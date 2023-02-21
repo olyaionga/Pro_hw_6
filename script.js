d468c5b6e3027385d8c23922138a0b60fdd57630
Olya Ionga <olechka2888@gmail.com>
2023-02-21 18:55:51 +0200

    update

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
-const hours = prompt("Введіть кількість годин:");
-const seconds = hours * 60 * 60;
-alert(`У ${hours} годинах ${seconds} секунд`);
+let hours = prompt("Введіть кількість годин:");
+let seconds = hours * 3600;
+alert(`${hours} годин дорівнює ${seconds} секунд`);
